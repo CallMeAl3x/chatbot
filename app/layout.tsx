7988f690218c4f995e543bc38b28813419ab8cac
CallMeAl3x <94863994+CallMeAl3x@users.noreply.github.com>
2024-12-04 23:30:40 -0500

    remove toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import "react-toastify/dist/ReactToastify.css";
-import { ToastContainer } from "react-toastify";
 import { SidebarProvider } from "./components/ui/sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -11,7 +9,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className={inter.className} suppressHydrationWarning>
         <SidebarProvider>
-          <ToastContainer position="top-left" />
           <main className="w-screen">{children}</main>
         </SidebarProvider>
       </body>
