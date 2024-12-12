c962ad39517f14caff4dbf97c6476e6e539c3e5f
CallMeAl3x <94863994+CallMeAl3x@users.noreply.github.com>
2024-12-12 11:03:26 -0500

    test

--- actions/logout.ts
+++ actions/logout.ts
@@ -6,6 +6,7 @@ import { signOut } from "@/auth";
 export async function logout() {
   try {
     await signOut();
+    window.location.reload();
     return { success: true, redirect: true };
   } catch (error) {
     return { error: "Erreur lors de la déconnexion" };
