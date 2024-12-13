7821f4187cd7d655ba85b858af2ccdfc5ad08e61
CallMeAl3x <94863994+CallMeAl3x@users.noreply.github.com>
2024-12-13 18:44:05 -0500

    fix logout on prod

--- actions/logout.ts
+++ actions/logout.ts
@@ -5,8 +5,8 @@ import { signOut } from "@/auth";
 
 export async function logout() {
   try {
-    await signOut();
-    return { success: true, redirect: true };
+    await signOut({ redirect: false });
+    return (window.location.href = "/");
   } catch (error) {
     return { error: "Erreur lors de la déconnexion" };
   }
