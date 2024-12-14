9d79923a133e060c8f4c114e1356fb6403841cc5
CallMeAl3x <94863994+CallMeAl3x@users.noreply.github.com>
2024-12-13 19:36:33 -0500

    added logout fixes

--- actions/logout.ts
+++ actions/logout.ts
@@ -1,12 +1,18 @@
-// actions/logout.ts
 "use server";
 
 import { signOut } from "@/auth";
 
 export async function logout() {
   try {
-    await signOut({ redirect: false });
-    return (window.location.href = "/");
+    await signOut();
+
+    // Utilisation de setTimeout pour ajouter un délai
+    await new Promise((resolve) => setTimeout(resolve, 500));
+
+    // Redirection côté client
+    if (typeof window !== "undefined") {
+      window.location.href = "/";
+    }
   } catch (error) {
     return { error: "Erreur lors de la déconnexion" };
   }
--- app/components/auth/logout.tsx
+++ app/components/auth/logout.tsx
@@ -1,28 +1,21 @@
 "use client";
 
 import { logout } from "@/actions/logout";
-import { useRouter } from "next/navigation";
 import { Button } from "../ui/button";
 
 export default function Logout() {
-  const router = useRouter();
-
   const handleLogout = async () => {
-    try {
-      await logout();
+    await logout();
+    setTimeout(() => {
       window.location.href = "/";
-    } catch (error) {
-      console.error("Erreur de déconnexion:", error);
-    }
+    }, 1250);
   };
 
   return (
-    <form action={handleLogout}>
-      <div className="flex justify-center">
-        <Button type="submit" className="bg-red-600 text-white mt-6 w-[90%] rounded-lg h-11 mb-4 font-semibold">
-          Sign out
-        </Button>
-      </div>
-    </form>
+    <div className="flex justify-center">
+      <Button onClick={handleLogout} className="bg-red-600 text-white mt-6 w-[90%] rounded-lg h-11 mb-4 font-semibold">
+        Sign out
+      </Button>
+    </div>
   );
 }
