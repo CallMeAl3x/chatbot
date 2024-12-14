74595894cb6c511efcdcd2ac1cbdb0e01e3c09c9
CallMeAl3x <94863994+CallMeAl3x@users.noreply.github.com>
2024-12-13 21:36:46 -0500

    yyyy

--- actions/logout.ts
+++ actions/logout.ts
@@ -5,7 +5,6 @@ import { signOut } from "@/auth";
 export const handleSignOut = async () => {
   try {
     await signOut({ redirectTo: "/", redirect: true });
-    window.dispatchEvent(new Event("session-changed"));
   } catch (error) {
     throw error;
   }
--- app/components/layout/AppLayout.tsx
+++ app/components/layout/AppLayout.tsx
@@ -1,29 +1,26 @@
+// components/layout/AuthLayout.tsx
 "use client";
 
-import { getSession, useSession } from "next-auth/react";
-import { useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { usePathname } from "next/navigation";
 import { AppSidebar } from "../ui/app-sidebar";
 import { SidebarProvider } from "../ui/sidebar";
 
 export function AuthLayout({ children }: { children: React.ReactNode }) {
-  const { data: session, update } = useSession(); // Récupère update pour forcer la mise à jour
+  const { data: session } = useSession();
+  const pathname = usePathname();
 
-  const shouldShowSidebar = session;
+  const isAuthPage = pathname.startsWith("/auth");
+  const isPublicHome = pathname === "/" && !session;
+  const shouldShowSidebar = !isAuthPage && !isPublicHome && session;
 
-  useEffect(() => {
-    const handleSessionChange = async () => {
-      await getSession(); // Recheck la session manuellement
-    };
-
-    window.addEventListener("session-changed", handleSessionChange);
-    return () => {
-      window.removeEventListener("session-changed", handleSessionChange);
-    };
-  }, [update]);
+  if (!shouldShowSidebar) {
+    return <>{children}</>;
+  }
 
   return (
     <SidebarProvider>
-      {shouldShowSidebar && <AppSidebar />}
+      <AppSidebar />
       <main className="w-full h-screen max-h-screen overflow-hidden bg-gray-50">{children}</main>
     </SidebarProvider>
   );
