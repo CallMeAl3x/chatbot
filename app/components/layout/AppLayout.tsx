69bf0a09abbed98defbdb72d3266adbb4715f651
CallMeAl3x <94863994+CallMeAl3x@users.noreply.github.com>
2024-12-15 13:15:23 -0500

    testt

--- app/components/layout/AppLayout.tsx
+++ app/components/layout/AppLayout.tsx
@@ -2,7 +2,6 @@
 
 import { useSession } from "next-auth/react";
 import { usePathname } from "next/navigation";
-import { useEffect } from "react";
 import { AppSidebar } from "../ui/app-sidebar";
 import { SidebarProvider } from "../ui/sidebar";
 
@@ -14,25 +13,14 @@ export function AuthLayout({ children }: { children: React.ReactNode }) {
   const isPublicHome = pathname === "/" && !session;
   const shouldShowSidebar = !isAuthPage && !isPublicHome && session;
 
-  useEffect(() => {
-    const handleSessionChange = () => {
-      console.log("'session-changed' capturé");
-      window.location.reload();
-    };
+  if (!shouldShowSidebar) {
+    return <>{children}</>;
+  }
 
-    window.addEventListener("session-changed", handleSessionChange);
-    return () => {
-      window.removeEventListener("session-changed", handleSessionChange);
-    };
-  }, []);
-
-  // Décidez quoi afficher dans le JSX, mais appelez toujours les hooks
-  return shouldShowSidebar ? (
+  return (
     <SidebarProvider>
       <AppSidebar />
       <main className="w-full h-screen max-h-screen overflow-hidden bg-gray-50">{children}</main>
     </SidebarProvider>
-  ) : (
-    <>{children}</>
   );
 }
